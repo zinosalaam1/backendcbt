6b43d8aae35db3f6a4036ba5addf631f8ba13bee
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-01-03 17:17:09 +0100

    commit

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,8 @@ const app = express();
 /* ✅ CORRECT CORS CONFIG */
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173'
+    'https://spcbt-nine.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
