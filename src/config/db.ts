1df7a893a8f18912220a03e920ddcb5aa28723d7
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-01-03 17:53:00 +0100

    commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = async (): Promise<void> => {
-  await mongoose.connect(process.env.MONGODB_URI as string);
-  console.log('MongoDB connected');
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+      throw new Error("MONGO_URI not defined");
+    }
+
+    await mongoose.connect(uri);
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
