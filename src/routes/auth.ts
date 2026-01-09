30e0f17d4f2aab304a0385ced36dc348ab4100fb
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-01-09 17:22:01 +0100

    first commit

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,30 +7,36 @@ import generateToken from "../utils/generateToken"; // ✅ default import
 const router = Router();
 
 router.post("/login", async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
-  if (!user) {
-    return res.status(401).json({ message: "Invalid credentials" });
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user._id.toString(), user.role);
+
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        role: user.role,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
+    res.status(500).json({ message: "Server error" });
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
-
-  const token = generateToken(user._id.toString());
-  res.json({
-  token,
-  user: {
-    id: user._id,
-    username: user.username,
-    role: user.role,
-    name: user.name,
-    email: user.email,
-  },
-});
-
 });
 
 export default router;
